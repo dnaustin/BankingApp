8b337579151e4ee229a146c17b41237e7f3f6170
Dave Austin <dave-austin@live.co.uk>
2023-07-14 18:12:44 +0100

    Create initial console app and authenticate with backend API

---
+++ ConsoleFrontEnd/ApiClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace ConsoleFrontEnd
+{
+    internal class ApiClient
+    {
+        private readonly HttpClient httpClient;
+        private string jwt;
+        
+        public ApiClient(string baseUri) 
+        {
+            httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(baseUri)
+            };
+        }
+
+        public async Task<bool> Authenticate(string userName, int pinCode)
+        {
+            using StringContent jsonContent = new StringContent(
+                JsonSerializer.Serialize(new { userName, pinCode }), Encoding.UTF8, "application/json");
+
+            using HttpResponseMessage response = await httpClient.PostAsync(
+                "api/auth/authenticate",
+                jsonContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                jwt = await response.Content.ReadAsStringAsync();
+            }
+
+            return response.IsSuccessStatusCode;
+        } 
+    }
+}
---
+++ ConsoleFrontEnd/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace ConsoleFrontEnd
+{
+    internal class Program
+    {
+        private static ApiClient apiClient = new ApiClient("http://localhost:61729");
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Account Management Console");
+
+            await Authenticate();
+        }
+
+        private static async Task Authenticate()
+        {         
+            bool authenticated = false;
+            
+            while (!authenticated)
+            {
+                string userName = GetUsername();
+                int pinCode = GetPinCode();
+
+                authenticated = await apiClient.Authenticate(userName, pinCode);
+                if (!authenticated)
+                {
+                    Console.WriteLine("Authentication failed, please try again.");
+                }
+            }   
+        }
+
+        private static string GetUsername()
+        {
+            string? userName;
+
+            while (true)
+            {
+                Console.WriteLine("Enter your username:");
+                userName = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(userName))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid username, please try again.");
+            }
+
+            return userName;
+        }
+
+        private static int GetPinCode()
+        {
+            int pinCode;
+            
+            while (true)
+            {
+                Console.WriteLine("Enter your pin code:");
+
+                if (int.TryParse(Console.ReadLine(), out pinCode))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid pin code, please try again.");
+            }
+
+            return pinCode;
+        }
+    }
+}
